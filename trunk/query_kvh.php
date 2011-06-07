90b59629a269e884fccfcd7740688a73d49b2ee1
aldridged@gmail.com <aldridged@gmail.com@324d418c-ae57-5b4e-c8b5-19f37415968d>
2011-06-07 16:06:11 +0000

    Initial implementation of KVH query code.

---
+++ trunk/query_kvh.php
@@ -0,0 +1,39 @@
+<?php
+// Query KVH units from KML file
+
+// Connect and retrieve KML data
+$ch = curl_init('http://208.83.165.114/KMLs/BDA44F00-6C56-4337-819D-DF4E8D6DE462/p45.kml');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10(KHTML, like Gecko) Chrome/8.0.552.237 Safari/534.10');
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, 'datacom:#ebbflow!');
+curl_setopt($ch, CURLOPT_PORT, 8080);
+$result = curl_exec($ch);
+
+// Convert returned XML to array
+$xml = simplexml_load_string($result);
+
+// Parse returned XML
+$index=0;
+foreach($xml->Folder->Placemark as $data) {
+  $cleancoords=explode(",",$data->Point->coordinates);
+  $cleanname=explode(" ",$data->name);
+  $cleandesc=explode("\n",$data->description);
+  $cleanstatus=trim(sprintf("%s",$data->description->p));
+  $cleanid=explode(" ",$cleandesc[11]);
+  $cleanspeed=explode(" ",$cleandesc[10]);
+  $kvhdata[$index]['name']=$cleanname[3];
+  $kvhdata[$index]['latitude']=$cleancoords[1];
+  $kvhdata[$index]['longitude']=$cleancoords[0];
+  $kvhdata[$index]['speed']=$cleanspeed[3];
+  $kvhdata[$index]['ipaddr']=$cleanname[0];
+  $kvhdata[$index]['id']=$cleanid[2];
+  $kvhdata[$index]['status']=$cleanstatus;
+  $kvhdata[$index]['notes']=$cleandesc[3]."\n".$cleandesc[5]."\n".$cleandesc[6];
+  $index++;
+};
+
+// Display
+print_r($kvhdata);
+
+?>
