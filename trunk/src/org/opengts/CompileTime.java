e5bc5734e1b05d27456a5a81bd27030e5736c423
aldridged@gmail.com <aldridged@gmail.com@324d418c-ae57-5b4e-c8b5-19f37415968d>
2011-06-07 21:30:29 +0000

    Attempt to enable reverse geocoding.

--- trunk/src/org/opengts/CompileTime.java
+++ trunk/src/org/opengts/CompileTime.java
@@ -1,6 +1,6 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2011/06/07 14:27:42 CDT
-    public static final long COMPILE_TIMESTAMP = 1307474862L;
+    // 2011/06/07 16:26:36 CDT
+    public static final long COMPILE_TIMESTAMP = 1307481996L;
 }
