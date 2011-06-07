45d783ce86c98fd62be89cdfa7f2aef95af0195c
aldridged@gmail.com <aldridged@gmail.com@324d418c-ae57-5b4e-c8b5-19f37415968d>
2011-06-07 19:52:19 +0000

    Updated jsmap.js file to not show empty fields and add notes to map pin balloons.

--- trunk/src/org/opengts/CompileTime.java
+++ trunk/src/org/opengts/CompileTime.java
@@ -1,6 +1,6 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2011/04/15 14:43:43 CDT
-    public static final long COMPILE_TIMESTAMP = 1302896623L;
+    // 2011/06/07 14:27:42 CDT
+    public static final long COMPILE_TIMESTAMP = 1307474862L;
 }
