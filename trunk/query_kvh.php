f8ab05d5740d4853c65c1c979daa677df5a06f74
aldridged@gmail.com <aldridged@gmail.com@324d418c-ae57-5b4e-c8b5-19f37415968d>
2011-07-12 16:19:27 +0000

    Updated KVH query code to not update the display name for existing devices - this is to allow devices to be renamed in GTS

--- trunk/query_kvh.php
+++ trunk/query_kvh.php
@@ -40,7 +40,7 @@ foreach($xml->Folder->Placemark as $data) {
 //Build Device and Location Insert Query
 $index=0;
 foreach($kvhdata as $data) {
-  $insertquery[$index] = "INSERT INTO Device (accountID,deviceID,groupID,equipmentType,vehicleID,uniqueID,displayName,description,isActive,lastUpdateTime,lastInputState,notes) VALUES ('gtg','".$data['id']."','kvh','netmodem','".$data['name']."','".$data['id']."','".$data['name']."','".$data['name']."',1,".time().",".$data['statuscode'].",'".$data['status']."<br />".$data['notes']."') ON DUPLICATE KEY UPDATE groupID=VALUES(groupID),lastUpdateTime=VALUES(lastUpdateTime),displayName=VALUES(displayName),description=VALUES(description),lastInputState=VALUES(lastInputState),notes=VALUES(notes);";
+  $insertquery[$index] = "INSERT INTO Device (accountID,deviceID,groupID,equipmentType,vehicleID,uniqueID,displayName,description,isActive,lastUpdateTime,lastInputState,notes) VALUES ('gtg','".$data['id']."','kvh','netmodem','".$data['name']."','".$data['id']."','".$data['name']."','".$data['name']."',1,".time().",".$data['statuscode'].",'".$data['status']."<br />".$data['notes']."') ON DUPLICATE KEY UPDATE groupID=VALUES(groupID),lastUpdateTime=VALUES(lastUpdateTime),description=VALUES(description),lastInputState=VALUES(lastInputState),notes=VALUES(notes);";
   $index++;
   $insertquery[$index] = "REPLACE INTO EventData SET accountID='gtg',deviceID='".$data['id']."',timestamp=".time().",statusCode=61472,latitude=".$data['latitude'].",longitude=".$data['longitude'].",speedKPH=".$data['speed'].";";
   $index++;
